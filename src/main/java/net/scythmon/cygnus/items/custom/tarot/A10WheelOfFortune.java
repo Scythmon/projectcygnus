2eacda8e8a47bba6ea7d0ae4f1aac8118c355f49
Scythmon <whisperingsythe@gmail.com>
2025-03-31 14:46:51 -0400

    This works a lot better now, used the registry to grab a random value by ID

--- src/main/java/net/scythmon/cygnus/items/custom/tarot/A10WheelOfFortune.java
+++ src/main/java/net/scythmon/cygnus/items/custom/tarot/A10WheelOfFortune.java
@@ -2,17 +2,20 @@ package net.scythmon.cygnus.items.custom.tarot;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.AreaEffectCloud;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.IForgeRegistry;
 import net.scythmon.cygnus.init.ModEffects;
 import org.apache.commons.compress.utils.Lists;
 
@@ -37,10 +40,8 @@ public class A10WheelOfFortune extends Item {
     }
 
     private void spawnEffectCloud(Level level, Player player) {
-        Registry<MobEffect> effectRegistry = (Registry<MobEffect>) ForgeRegistries.MOB_EFFECTS;
-        List<MobEffect> effects =  effectRegistry.stream().toList();
         Random random = new Random();
-        MobEffect randomEffect = effects.get(random.nextInt(effects.size()));
+        MobEffect randomEffect = MobEffect.byId(random.nextInt(ForgeRegistries.MOB_EFFECTS.getValues().size()));
         AreaEffectCloud cloud = new AreaEffectCloud(level, player.getX(), player.getY(), player.getZ());
         cloud.setRadius(2.5F);
         cloud.setRadiusOnUse(-0.5F);
